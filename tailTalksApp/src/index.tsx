f15935d27b64424baea7263664e85f56b512fa9d
f1redes1re <le.xander.gl@gmail.com>
2024-08-02 22:12:26 +0200

    [#86bzkkd9h] Добавлен блок Протяните руку вашему питомцу с NFT + внесены исправления в Хедер

--- tailTalksApp/src/index.tsx
+++ tailTalksApp/src/index.tsx
@@ -2,14 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import Header from "./Header/Header"
+import Main from "./Main/Main"
 
 const rootElement = document.getElementById("root")
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
-    <React.StrictMode>
+    <>
       <Header />
-    </React.StrictMode>,
+      <Main />
+    </>,
   )
 } else {
   console.error("Root element not found")
