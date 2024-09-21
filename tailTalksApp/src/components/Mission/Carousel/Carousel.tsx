46e42fe170c168d50c4b5d5c746e2c8488ff4c34
Alexander Glebov <le.xander.gl@gmail.com>
2024-09-21 18:54:41 +0200

    [#86c09cyf5] Сделан блок Эксклюзив + произведен рефакторинг блоков Хедер, Мэйн, Миссия, Философия, Комьюнити, Футер - добавлена адаптивная верстка за исключением мобилки (будет сделана дополнительно)

--- tailTalksApp/src/components/Mission/Carousel/Carousel.tsx
+++ tailTalksApp/src/components/Mission/Carousel/Carousel.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react"
+import "../Mission.css"
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
@@ -25,7 +26,7 @@ const Carousel: React.FC = () => {
   return (
     <div className="w-full">
       <div className="mb-[30px] flex items-center justify-between">
-        <h2 className="text-[40px] font-semibold leading-[72px] text-white">
+        <h2 className="font-semibold text-white lg:text-[35px] xl:text-[40px] xl:leading-[72px]">
           Наша миссия
         </h2>
         <div className="flex w-[150px] justify-end">
@@ -36,18 +37,18 @@ const Carousel: React.FC = () => {
       <div className="overflow-hidden">
         <Slider ref={sliderRef} {...settings}>
           <div className="">
-            <div className="mission__slider-card mr-[30px]">
-              <div className="mission__slider-card-inner flex h-[515px] w-[407px] flex-col items-center">
+            <div className="mission__slider-card lg:mr-[40px] xl:mr-[30px]">
+              <div className="mission__slider-card-inner rounded-[20px] sm:h-[435px] sm:w-[367px] lg:p-[30px] xl:h-[515px] xl:w-[407px] xl:px-[26px] xl:py-[40px]">
                 <img
-                  className="mission__slider-card mb-[28px] h-[250px] w-[355px]"
+                  className="mission__slider-card mb-[28px] lg:h-[225px] lg:w-[307px] xl:h-[250px] xl:w-[355px]"
                   src={pic1}
                   alt="image1"
                 ></img>
-                <div className="flex flex-col items-center justify-center mb-auto">
-                  <div className="mb-[17px] text-[25px] font-semibold leading-[40px] text-white">
+                <div className="flex flex-col items-center justify-center">
+                  <div className="mb-[17px] font-semibold text-white lg:text-xl xl:text-2xl">
                     Объединить
                   </div>
-                  <p className="text-center text-[20px] font-thin text-white">
+                  <p className="text-center font-light text-white lg:text-base xl:text-xl">
                     сообщество владельцев питомцев со всего мира
                   </p>
                 </div>
@@ -55,18 +56,18 @@ const Carousel: React.FC = () => {
             </div>
           </div>
           <div className="">
-            <div className="mission__slider-card mr-[30px]">
-              <div className="mission__slider-card-inner h-[515px] w-[407px] rounded-[20px]">
+            <div className="mission__slider-card lg:mr-[40px] xl:mr-[30px]">
+              <div className="mission__slider-card-inner rounded-[20px] sm:h-[435px] sm:w-[367px] lg:p-[30px] xl:h-[515px] xl:w-[407px] xl:px-[26px] xl:py-[40px]">
                 <img
-                  className="mission__slider-card mb-[28px] h-[250px] w-[355px]"
+                  className="mission__slider-card mb-[28px] lg:h-[225px] lg:w-[307px] xl:h-[250px] xl:w-[355px]"
                   src={pic2}
                   alt="image2"
                 ></img>
-                <div className="flex flex-col items-center justify-center mb-auto">
-                  <div className="mb-[17px] text-[25px] font-semibold leading-[40px] text-white">
+                <div className="flex flex-col items-center justify-center">
+                  <div className="mb-[17px] font-semibold text-white lg:text-xl xl:text-2xl">
                     Популяризировать
                   </div>
-                  <p className="text-center text-[20px] font-thin text-white">
+                  <p className="text-center font-light text-white lg:text-base xl:text-xl">
                     технологии блокчейна — фото вашего питомца станет NFT!
                   </p>
                 </div>
@@ -74,18 +75,18 @@ const Carousel: React.FC = () => {
             </div>
           </div>
           <div className="">
-            <div className="mission__slider-card mr-[30px]">
-              <div className="mission__slider-card-inner h-[515px] w-[407px] rounded-[20px]">
+            <div className="mission__slider-card lg:mr-[40px] xl:mr-[30px]">
+              <div className="mission__slider-card-inner rounded-[20px] sm:h-[435px] sm:w-[367px] lg:p-[30px] xl:h-[515px] xl:w-[407px] xl:px-[26px] xl:py-[40px]">
                 <img
-                  className="mission__slider-card mb-[28px] h-[250px] w-[355px]"
+                  className="mission__slider-card mb-[28px] lg:h-[225px] lg:w-[307px] xl:h-[250px] xl:w-[355px]"
                   src={pic3}
                   alt="image3"
                 ></img>
-                <div className="flex flex-col items-center justify-center mb-auto">
-                  <div className="mb-[17px] text-[25px] font-semibold leading-[40px] text-white">
+                <div className="flex flex-col items-center justify-center">
+                  <div className="mb-[17px] font-semibold text-white lg:text-xl xl:text-2xl">
                     Создать
                   </div>
-                  <p className="text-center text-[20px] font-thin text-white">
+                  <p className="text-center font-light text-white lg:text-base xl:text-xl">
                     безопасное и интерактивное пространство для общения и обмена
                     опытом
                   </p>
@@ -94,18 +95,18 @@ const Carousel: React.FC = () => {
             </div>
           </div>
           <div className="">
-            <div className="mission__slider-card mr-[30px]">
-              <div className="mission__slider-card-inner h-[515px] w-[407px] rounded-[20px]">
+            <div className="mission__slider-card lg:mr-[40px] xl:mr-[30px]">
+              <div className="mission__slider-card-inner rounded-[20px] sm:h-[435px] sm:w-[367px] lg:p-[30px] xl:h-[515px] xl:w-[407px] xl:px-[26px] xl:py-[40px]">
                 <img
-                  className="mission__slider-card mb-[28px] h-[250px] w-[355px]"
+                  className="mission__slider-card mb-[28px] lg:h-[225px] lg:w-[307px] xl:h-[250px] xl:w-[355px]"
                   src={pic4}
                   alt="image4"
                 ></img>
-                <div className="flex flex-col items-center justify-center mb-auto">
-                  <div className="mb-[17px] text-[25px] font-semibold leading-[40px] text-white">
+                <div className="flex flex-col items-center justify-center">
+                  <div className="mb-[17px] font-semibold text-white lg:text-xl xl:text-2xl">
                     Поддержать
                   </div>
-                  <p className="text-center text-[20px] font-thin text-white">
+                  <p className="text-center font-light text-white lg:text-base xl:text-xl">
                     благотворительные инициативы, помогающие животным в беде
                   </p>
                 </div>
